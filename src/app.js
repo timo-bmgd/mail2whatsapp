0bbc4993e74f5d1be5b415ee0d247aed456cfeee
ultratynoz <tynoz@web.de>
2023-05-06 18:10:23 +0200

    added whatsapp auth functionality

--- src/app.js
+++ src/app.js
@@ -1,1 +1,33 @@
-console.log("test 2")
+const qrcode = require('qrcode-terminal');
+
+const { Client, LocalAuth } = require('whatsapp-web.js');
+
+const authStrategy = new LocalAuth({
+    clientId: "client"
+})
+
+const client = new Client({
+    takeoverOnConflict: true,
+    authStrategy,
+});
+ 
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('message_create', async (msg)=>{
+   const groupChat = await msg.getChat()
+   if(groupChat.isGroup && groupChat.name === "Mail" && msg.body === "!mail") {
+        msg.reply("Getting mails!");
+   }
+})
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+ 
+
+client.initialize();
+ 
+ 
