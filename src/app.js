933efd42793245e6c29b0a5c3a9f7e8c661192af
ultratynoz <tynoz@web.de>
2023-05-07 13:51:58 +0200

    In collaboration with Melwin Kramer https://github.com/Melwiin we added the connection between mail fetching and sending mails via whatsapp and finished the functionality of the script

--- src/app.js
+++ src/app.js
@@ -1,33 +1,40 @@
-const qrcode = require('qrcode-terminal');
+const qrcode = require("qrcode-terminal");
 
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const { Client, LocalAuth } = require("whatsapp-web.js");
+const fetchEmails = require("./mail");
 
 const authStrategy = new LocalAuth({
-    clientId: "client"
-})
+    clientId: "client",
+});
 
 const client = new Client({
     takeoverOnConflict: true,
     authStrategy,
 });
- 
 
-client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
+client.on("qr", (qr) => {
+    qrcode.generate(qr, { small: true });
 });
 
-client.on('message_create', async (msg)=>{
-   const groupChat = await msg.getChat()
-   if(groupChat.isGroup && groupChat.name === "Mail" && msg.body === "!mail") {
-        msg.reply("Getting mails!");
-   }
-})
-
-client.on('ready', () => {
-    console.log('Client is ready!');
+client.on("message_create", async (msg) => {
+    const groupChat = await msg.getChat();
+    let command = msg.body.split(" ");
+    let argument = command[1] || 1;
+    if (groupChat.isGroup && groupChat.name === "Mail") {
+        if (command[0] == "!mail") {
+            if (isNaN(argument))
+                return client.sendMessage(msg.from, "Invalid number of emails!");
+            let fetchedEmails = await fetchEmails(argument).catch(err => console.log(err));
+            msg.reply(`Fetching last ${fetchedEmails.length} emails!`);
+            fetchedEmails.forEach((mail) => {
+                client.sendMessage(msg.from, mail);
+            });
+        }
+    }
 });
- 
+
+client.on("ready", () => {
+    console.log("Client is ready!");
+});
 
 client.initialize();
- 
- 
---
+++ src/mail.js
@@ -0,0 +1,77 @@
+var Imap = require("imap"),
+  inspect = require("util").inspect;
+var htmlToText = require("html-to-text");
+
+var imap = new Imap({
+  user: process.env.EMAIL_USERNAME,
+  password: process.env.EMAIL_PASSWORD,
+  host: process.env.IMAP_SERVER,
+  port: process.env.IMAP_PORT || 993,
+  tls: true,
+});
+
+function openInbox(cb) {
+  imap.openBox("INBOX", true, cb);
+}
+
+function fetchEmails(emailCount) {
+  return new Promise(function(resolve, reject) {
+  let allMsgs = [];
+  imap.once("ready", function () {
+    openInbox(function (err, box) {
+      if (err) throw err;
+      let requestedNumber = Math.min(box.messages.total - 1,(emailCount - 1));
+      var f = imap.seq.fetch(
+        box.messages.total -  requestedNumber + ":" + box.messages.total,
+        { bodies: ["HEADER.FIELDS (FROM)", "1"], struct: true });
+        
+      f.on("message", function (msg, seqno) {
+        let result = "";
+        //console.log("Message #%d", seqno);
+        var prefix = "(#" + seqno + ") ";
+        msg.on("body", function (stream, info) {
+          var buffer = "", count = 0;
+          stream.on("data", function (chunk) {
+            count += chunk.length;
+            buffer += chunk.toString("utf8");
+          });
+          stream.once("end", function () {
+            let textBody = buffer;
+
+            result += htmlToText.convert(textBody, {}) + "\n";
+
+            if (info.which !== "TEXT")
+              Imap.parseHeader(buffer);
+          });
+        });
+        msg.once("attributes", function (attrs) {
+          //console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
+        });
+        msg.once("end", function () {
+          allMsgs.push(result);
+        });
+      });
+      f.once("error", function (err) {
+        console.log("Fetch error: " + err);
+      });
+      f.once("end", function () {
+        console.log("Done fetching all messages!");
+        imap.end();
+        resolve(allMsgs);
+      });
+    });
+  });
+
+  imap.once("error", function (err) {
+    console.log(err);
+    reject();
+  });
+
+  imap.once("end", function () {
+    console.log("Connection ended");
+  });
+
+  imap.connect();
+})}
+
+module.exports = fetchEmails;
